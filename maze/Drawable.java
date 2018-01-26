d5aac5529deecfbfe0becc0b55f7f2773fb55f88
themcducky <themcducky@gmail.com>
2018-01-26 16:14:34 +0100

    NA

--- maze/Drawable.java
+++
@@ -1,5 +0,0 @@
-package com.mcducky.maze;
-
-public interface Drawable {
-    void draw(Display img);
-}
--- maze/GameObject.java
+++
@@ -1,5 +0,0 @@
-package com.mcducky.maze;
-
-public abstract class  GameObject {
-    public abstract void update();
-}
